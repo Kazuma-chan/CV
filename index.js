c790b15549f794d33e19fc6ef2bcb4b6a7c82807
Kazuma-chan <156342525+Kazuma-chan@users.noreply.github.com>
2024-08-07 13:38:57 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const incrementBtn = document.getElementById("increment-btn")
-const countDisplay = document.getElementById("count-display")
-
-incrementBtn.addEventListener('click', ()=>{
-    alert("clicked")
-})
